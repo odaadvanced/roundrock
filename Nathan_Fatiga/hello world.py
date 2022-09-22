2601d1c32f7827a4b47d78955131a276cfc09158
odaadvanced <stbaoda@gmail.com>
2022-09-22 16:07:04 -0500

    class nots

--- Nathan_Fatiga/hello world.py
+++ Nathan_Fatiga/hello world.py
@@ -1,1 +1,11 @@
-print("Hello World")
+password = "NaThAn11"
+username = "Nathan Fatiga"
+u = input("insert username here: ")
+if u == username:
+    p = input("insert password here: ")
+    if p == password:
+        print("Welcome back", username)
+    else:
+        print("error")
+else:
+    print("error")
