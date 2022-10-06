11c684c53deddddcaa805ab91fd81bbd090cbebb
odaadvanced <stbaoda@gmail.com>
2022-10-06 15:57:13 -0500

    adfa

---
+++ Nathan_Fatiga/POLITE.py
@@ -0,0 +1,5 @@
+def make_polite(sentance):
+    polite_sentance = sentance + " please"
+    return polite_sentance
+
+print(make_polite("pass the salt"))
---
+++ Nathan_Fatiga/strings.py
@@ -0,0 +1,27 @@
+import random
+rn = random.randint(1, 13)
+print(rn)
+my_name = "Nathan Fatiga"
+print("Welcome Back", my_name, "!!!")
+print(len(my_name))
+int(rn)
+print(my_name[rn])
+my_class = "Advanced Robotics Class"
+print("Welcome Back", my_class, "Student #", rn, "!!!")
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(num)
+print(num[1:3])
+num[2] = 15
+print(num)
+more_numbers = [2, 3, 4, 1000]
+print(more_numbers)
+newnum = num + more_numbers
+print(newnum)
+newnum.sort()
+print(newnum)
+newnum.pop(2 and 5)
+print(newnum)
+newnum.append(3245)
+print(newnum)
+newnumn = newnum.count(4)
+print(newnumn)
