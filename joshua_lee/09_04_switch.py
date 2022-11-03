4e3799d8a7ef5ca0c9cab740c17348d647a1976c
odaadvanced <stbaoda@gmail.com>
2022-11-03 15:52:38 -0500

    hello

---
+++ joshua_lee/09_04_switch.py
@@ -0,0 +1,11 @@
+#09_04_switch.py
+
+import gpiozero, time
+
+switch = gpiozero.Button(23, pull_up=True)
+
+while True:
+    if switch.is_pressed:
+        
+        print("Button Pressed")
+        time.sleep(0.5)
