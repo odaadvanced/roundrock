81a16de84a73a71a1af25740c1c42ebcacb19e0f
odaadvanced <stbaoda@gmail.com>
2022-11-17 15:45:12 -0600

    hello

---
+++ joshua_lee/01_blink.py
@@ -0,0 +1,14 @@
+# 01_blink.py
+# From the code for the Box 1 kit for the Raspberry Pi by MonkMakes.com
+
+from gpiozero import LED
+from signal import pause
+
+red_led = LED(17)
+
+# The first number in blink() is the on time and the second is the off time (both in seconds)
+red_led.blink(on_time=0.5, off_time=0.5)
+
+pause()
+
+# The pause() is needed at the end to keep the program running.
