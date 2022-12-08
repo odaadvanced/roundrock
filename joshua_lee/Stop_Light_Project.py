6f1282fa78c322dd5cfebca71d61bb6915480356
odaadvanced <stbaoda@gmail.com>
2022-12-08 16:00:18 -0600

    hello

---
+++ joshua_lee/Stop_Light_Project.py
@@ -0,0 +1,21 @@
+from gpiozero import LED
+from gpiozero import TrafficLights
+from time import sleep
+
+lights = TrafficLights(22, 27, 17)
+
+lights.green.on()
+
+while True:
+    sleep(10)
+    lights.green.off()
+    lights.amber.on()
+    sleep(1)
+    lights.amber.off()
+    lights.red.on()
+    sleep(10)
+    lights.amber.on()
+    sleep(1)
+    lights.green.on()
+    lights.amber.off()
+    lights.red.off()
