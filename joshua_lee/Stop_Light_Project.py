327fbef8b66ad26f2d68b8f8752f68310b41250e
odaadvanced <stbaoda@gmail.com>
2023-01-12 15:54:53 -0600

    hello

--- joshua_lee/Stop_Light_Project.py
+++ joshua_lee/Stop_Light_Project.py
@@ -15,6 +15,18 @@ bz.off()
 
 lights.red.on()
 
+def buzz(pitch, duration):
+    period = 1.0 / pitch
+    p2 = period / 2
+    cycles = int(duration * pitch)
+    for i in range(0, cycles):
+        pin1.on()
+        pin2.off()
+        delay(p2)
+        pin1.off()
+        pin2.on()
+        delay(p2)
+
 while True:
     if button.is_pressed:
         display.print("15")
