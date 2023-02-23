0a436fef4143ed2a1111937eece879a3e9700e5d
odaadvanced <stbaoda@gmail.com>
2023-02-23 15:57:06 -0600

    new files

---
+++ Evan/for_else_loop.py
@@ -0,0 +1,7 @@
+phrase = "it marks the spot"
+for character in phrase:
+    if character == "X":
+        print ("X found")
+        break
+else:
+    print("there was no 'X' in the phrase")
---
+++ Evan/if_statement_test.py
@@ -0,0 +1,14 @@
+grade  = 65
+if grade > 100:
+    print("You passed the class with an S!")
+elif grade >= 96:
+    print("You passed the class with an A+")
+elif grade >= 93:
+    print("You passed the class with an A!")
+elif grade >=90
+    print("You passed the class with an A-")
+elif grade >=85
+    print("You passed the class with a B")
+else:
+    print ("You did not pass the class : (")
+print("Thank you for attending")
